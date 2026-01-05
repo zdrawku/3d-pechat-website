91c39819c7de733a525f4d50a12e98191692ac81
zdrawku <zkolev@infragistics.com>
2026-01-05 18:20:08 +0200

    Add new images to carousel and general image assets; enhance layout responsiveness

--- src/app/main-page/carousel-images.ts
+++ src/app/main-page/carousel-images.ts
@@ -2,6 +2,8 @@
 // Run 'npm run generate-images' to regenerate this file.
 
 export const carouselImages: string[] = [
+  '/assets/real-images/first-page-images/1.png',
+  '/assets/real-images/first-page-images/2.png',
   '/assets/real-images/first-page-images/20250109_204217.jpg',
   '/assets/real-images/first-page-images/20250201_145113.jpg',
   '/assets/real-images/first-page-images/20250517_164324.jpg',
@@ -9,5 +11,6 @@ export const carouselImages: string[] = [
   '/assets/real-images/first-page-images/20251213_195315.jpg',
   '/assets/real-images/first-page-images/20251221_104554.jpg',
   '/assets/real-images/first-page-images/20251227_201301.jpg',
+  '/assets/real-images/first-page-images/3.png',
   '/assets/real-images/first-page-images/IMG-4f0120455eb7ebacb61f9d579dae2fa3-V.jpg'
 ];
